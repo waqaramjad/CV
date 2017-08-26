7267879d3322c8d9fa9f5f0214c1a2c9c377453e
waqaramjad <waqaramjad345@gmail.com>
2017-08-26 18:16:55 +0500

    components building done +implementing components in Template

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,41 @@
 
 var resumeData = document.getElementsByClassName('resumeData');
 
-
+console.log(resumeData);
 var resume =  {
 // main page skills
     name : 'Waqar Amjad',
     mainPageSkills: 'Software Developer | Web Developer | Mobile Developer',
-// about us
-    about:'I am waqar Amjad , student of Softwate Engineering from Karachi Unievrsity ,Ubit. Passionate about to learn and work on new technology .I have a experience in Java and we projects',
-// resume 
-    university :' University Of Karachi,Ubit <br> BS(Software Engineering) <br>2016-2019',
-    College: 'Bahria College NORE-1 , Karachi <br>Pre-Enginnering<br>2013-2015' ,
+    about:'I am waqar amjad currently the student of University oF KARACHI , UBIT . I am self larner and passionate about new technology , also try to learn somethoing new and try to explore and innovete new thing .I Do my my work with Honestly and hardworking ',
+skills:'i have skills of programming , Web and Mobile Application ',
+html:'I learn HTMl from Jhon ducket book and have a complete knowledge of all Html tags  ',   
+css:'I learn Css from John ducket Book and have a command on all CSS tags',
+js:'I learn Java Script from Smarter way to learn JavaSCript  ',
+java:'i make many programs and projects in java language including coaching mangment system forn a coaching',
+csharp:'I make many project on windows form application including Textile managment System',
+c:'I make many programms in C language and also have a command on Data Structures and time complexity of Algorithem',
+profolio:'I have command on programming Languages , Java , C# , C , JS also i make many projects in web and Desktop Appllication .',
+education:'I am currently the student of UBIT , University Of karachi.<br>I am doing software Ebgineering and i want to be specilize in Artificial Inteligence .',
+School:'EAB PN Sec School',
+College:'Bahria Collge Karachi',
+Collegefac:' Pre-Engineering',
+CollegeEra:'Aprail 2013 - March 2015',
+university:'UBIT , University Of Karachi',
+universityFac:'BS(Software Enginering)',
+universityEra:'Jan 2016 - DEC 2019',
+Saylani:'Saylani Mobile Apllication Development',
+SaylaniFac:'Android , IOS , WEb , React',
+SaylaniEra:'Nov 2016 - March 2018',
+QuoteMusk:"'Some people don't like change, but you need to embrace change if the alternative is disaster. <cite>Elon Musk<br/><i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i></cite>",
+QuoteSteve:"Being the richest man in the cemetery doesn't matter to me. Going to bed at night saying we've done something wonderful, that's what matters to me. <cite>Steve Jobs<br/><i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i></cite>",
+adress:'Haroon Bharia, Society , Naval , Karachi',
+phoneNo:'+92366984223',
+email:'waqaramjad345@gmail.com',
+
+
+
+university :' University Of Karachi,Ubit <br> BS(Software Engineering) <br>2016-2019',
+    // College: 'Bahria College NORE-1 , Karachi <br>Pre-Enginnering<br>2013-2015' ,
     School : 'EAB PN Haroon Bahria Colleg , Karachi',
     Saylani :'Mobile and web App Development using React',
 //  location
@@ -21,10 +46,35 @@ var resume =  {
 resumeData[0].innerHTML = resume.name;
 resumeData[1].innerHTML = resume.mainPageSkills;
 resumeData[2].innerHTML = resume.about;
-resumeData[3].innerHTML = resume.university;
-resumeData[4].innerHTML = resume.College;
-resumeData[5].innerHTML = resume.School;
-resumeData[6].innerHTML = resume.Saylani;
+
+resumeData[3].innerHTML = resume.skills
+resumeData[4].innerHTML = resume.html
+resumeData[5].innerHTML = resume.css
+resumeData[6].innerHTML = resume.js
+resumeData[7].innerHTML = resume.java
+resumeData[8].innerHTML = resume.csharp
+resumeData[9].innerHTML = resume.c
+resumeData[10].innerHTML = resume.profolio
+resumeData[11].innerHTML = resume.education
+resumeData[12].innerHTML = resume.School
+resumeData[13].innerHTML = resume.College
+resumeData[14].innerHTML = resume.Collegefac
+resumeData[15].innerHTML = resume.CollegeEra
+resumeData[16].innerHTML = resume.university
+resumeData[17].innerHTML = resume.universityFac
+resumeData[18].innerHTML = resume.universityEra
+resumeData[19].innerHTML = resume.Saylani
+resumeData[20].innerHTML = resume.SaylaniFac
+resumeData[21].innerHTML = resume.SaylaniEra
+resumeData[22].innerHTML = resume.QuoteMusk
+resumeData[23].innerHTML = resume.QuoteSteve
+resumeData[24].innerHTML = resume.adress
+resumeData[25].innerHTML = resume.phoneNo
+resumeData[26].innerHTML = resume.email
+// resumeData[26].innerHTML = resume.
+// resumeData[2].innerHTML = resume.
+// resumeData[2].innerHTML = resume.
+
+
 // resumeData[7].innerHTML = resume.livingPlace;
 
-console.log(resumeData);
